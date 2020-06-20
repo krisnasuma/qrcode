472d57bfe1dfd90f0cfc29d7723eed45f707d4db
krisnasuma <45704616+krisnasuma@users.noreply.github.com>
2020-06-20 22:17:38 +0800

    Create webview_screen.dart

---
+++ webview_screen.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:webview_flutter/webview_flutter.dart';
+
+class WebScreen extends StatelessWidget {
+  final String url;
+
+  WebScreen(this.url);
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(url),
+        centerTitle: true,
+        backgroundColor: Colors.indigo,
+      ),
+      body: WebView(
+        initialUrl: url,
+        javascriptMode: JavascriptMode.unrestricted,
+      ),
+    );
+  }
+}
